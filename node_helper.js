5a540826474a11bd0a4670248164f03fe3ef282a
Max Brosnahan <maximilianbrosnahan@gmail.com>
2020-06-13 12:59:49 +0100

    Use UK tide times to get tide information

--- node_helper.js
+++ node_helper.js
@@ -16,13 +16,59 @@ module.exports = NodeHelper.create({
     },
 
     getTides: function(url) {
+        d = {
+            responseLat: "",
+            responseLon: "",
+            station: "methil",
+            extremes: [
+                {
+                dt: "", //	Date/Time of this extreme (in seconds since the unix epoch).
+                date: "", //Date/Time of this extreme (in ISO 8601 standard date and time format, e.g.: 2017-06-12T19:47+0000 ).
+                height: 1, //Height (in meters) of the tide.
+                type: "High",
+                },
+            ],
+        };
+        // request({
+        //     url: url,
+        //     method: 'GET'
+        // }, (error, response, body) => {
+        //     if (!error && response.statusCode == 200) {
+        //         var result = JSON.parse(body);
+		// 	//	console.log(response.statusCode + result); // for checking
+        //         this.sendSocketNotification('TIDES_RESULT', result);
+        //     }
+        // });
+        url = "https://www.tidetimes.org.uk/methil-tide-times"
+
+
         request({
             url: url,
             method: 'GET'
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-			//	console.log(response.statusCode + result); // for checking
+                const tideData  = body.match(/High Tide(.*?)<\/tr>/);
+                const extremes = tideData.map((rawData) => {
+                    const time = rawData.match(/\d\d:\d\d/)[0];
+                    const height = parseFloat(rawData.match(/\d+\.\d+m/)[0]);
+                    const tideTime =  new Date()
+                    const [hours, minutes] = time.split(":")
+                    tideTime.setHours(hours.parseInt(hours - 1))
+                    tideTime.setMinutes(hours.parseInt(minutes))
+                    return {
+                        dt: tideTime.getTime(), //	Date/Time of this extreme (in seconds since the unix epoch).
+                        date: tideTime.toISOString(), //Date/Time of this extreme (in ISO 8601 standard date and time format, e.g.: 2017-06-12T19:47+0000 ).
+                        height: height, //Height (in meters) of the tide.
+                        type: "High",
+                    }
+                })
+                const result = {
+                    responseLat: "",
+                    responseLon: "",
+                    station: "methil",
+                    extremes,
+                };
+				console.log(response.statusCode, result); // for checking
                 this.sendSocketNotification('TIDES_RESULT', result);
             }
         });
